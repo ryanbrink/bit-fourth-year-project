df1f965492e49207e0a383e10dd3cc1b3187349e
Heathton <ryan.mitchell.heath@gmail.com>
2017-01-12 12:09:26 -0500

    Command line
    Creating Command line interface

--- src/com/net4901/firewall/CommandLine.java
+++ src/com/net4901/firewall/CommandLine.java
@@ -2,6 +2,7 @@ package com.net4901.firewall;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Authenticator;
@@ -12,7 +13,7 @@ import java.net.URL;
 
 public class CommandLine {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		for (int i = 0; i < args.length; i++) {
 			// Parse command line arguments into FirewallRules
 		}
@@ -25,7 +26,7 @@ public class CommandLine {
 		testRule.destinationNetwork = "192.168.2.3";
 		testRule.destinationMask = "32";
 		testRule.isDenied = true;
-		
+		System.out.println("Test");
 		manager.addRule(testRule);
 	}
 }
